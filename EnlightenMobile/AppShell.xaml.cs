56bb72b619030456a7c381aaeeadb0e9497e8c9a
Mark Zieg <mark@zieg.com>
2021-02-12 12:46:58 -0500

    capturing early Shell testing

---
+++ EnlightenMobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EnlightenMobile
+{
+    /// <summary>
+    /// Defines application-wide navigation in XAML.
+    /// </summary>
+    /// <see cref="https://www.youtube.com/watch?v=0y1bUAcOjZY"/>
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
