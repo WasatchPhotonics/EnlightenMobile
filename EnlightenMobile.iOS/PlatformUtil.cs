b4f391ae3085da666c96c666a0295183c3be7db1
Evan Dort <edort@wasatchphotonics.onmicrosoft.com>
2021-05-19 09:04:21 -0400

    fix flyout layout, implement iOS save

--- EnlightenMobile.iOS/PlatformUtil.cs
+++ EnlightenMobile.iOS/PlatformUtil.cs
@@ -3,6 +3,7 @@ using Xamarin.Forms;
 using EnlightenMobile.Services;
 using EnlightenMobile;
 using UIKit;
+using System.IO;
 using Xamarin.Forms.Platform.iOS;
 using CoreGraphics;
 
@@ -12,6 +13,7 @@ namespace EnlightenMobile.iOS
     public class PlatformUtil : IPlatformUtil
     {
         Logger logger = Logger.getInstance();
+        string savePath = null;
         
         public PlatformUtil() { }
 
@@ -34,34 +36,47 @@ namespace EnlightenMobile.iOS
         //
         // @see https://stackoverflow.com/a/42844156/11615696
         public void toast(string message, View view = null)  
-        {  
-            if (view is null)
-                return;
+        {
+            UIView viewPresent = null;
+            // None of the toast invokes pass a view so this doesn't work unless we get a view
+            // see https://forums.xamarin.com/discussion/24689/how-to-acces-the-current-view-uiviewcontroller-from-an-external-service
+            var window = UIApplication.SharedApplication.KeyWindow;
+            var vc = window.RootViewController;
+            //while (vc.PresentedViewController != null)
+            //{
+            //    vc = vc.PresentedViewController;
+            //}
+            viewPresent = vc.View;
+            
 
+            if(viewPresent is null)
+            {
+                return;
+            }
             // PageNav pageNav = PageNav.getInstance();
             // var currentPage = pageNav.tabbedPage.CurrentPage;
 
-            UIView uiView = viewToUIView(view);
+            UIView uiView = viewPresent;
 
             UIView residualView = uiView.ViewWithTag(1989);
             if (residualView != null)
                 residualView.RemoveFromSuperview();
 
-            var viewBack = new UIView(new CoreGraphics.CGRect(83, 0, 300, 100));
-            viewBack.BackgroundColor = UIColor.Black;
+            var viewBack = new UIView(new CoreGraphics.CGRect(0, 0, 300, 60)); //size shape of box
+            viewBack.BackgroundColor = UIColor.White;
             viewBack.Tag = 1989;
-            UILabel lblMsg = new UILabel(new CoreGraphics.CGRect(0, 20, 300, 60));
+            UILabel lblMsg = new UILabel(new CoreGraphics.CGRect(0, 0, 300, 60)); //size shape of text
             lblMsg.Lines = 2;
             lblMsg.Text = message;
-            lblMsg.TextColor = UIColor.White;
+            lblMsg.TextColor = UIColor.Black;
             lblMsg.TextAlignment = UITextAlignment.Center;
-            viewBack.Center = uiView.Center;
+            viewBack.Center = new CGPoint(x: uiView.Bounds.GetMidX(), y: uiView.Bounds.GetMaxY() - 50); //positioning of box
             viewBack.AddSubview(lblMsg);
             uiView.AddSubview(viewBack);
             viewBack.Layer.CornerRadius=10; 
             viewBack.Layer.MasksToBounds=true;
             UIView.BeginAnimations("Toast");
-            UIView.SetAnimationDuration(3.0f);
+            UIView.SetAnimationDuration(4.0f);
             viewBack.Alpha = 0.0f;
             UIView.CommitAnimations();
         }
@@ -69,7 +84,34 @@ namespace EnlightenMobile.iOS
         // @todo implement on iOS
         public string getSavePath()
         {
-            return null;
+             if (savePath != null)
+            {
+                logger.debug($"getSavePath: returning previous savePath {savePath}");
+                return savePath;
+            }
+
+            // create EnlightenSpectra if necessary
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var defaultPath = documents;
+            logger.debug($"getSavePath: defaultPath = {defaultPath}");
+            if (!writeable(defaultPath))
+            {
+                logger.error($"getSavePath: unable to write defaultPath {defaultPath}");
+                return null;
+            }
+
+            // create today if necessary
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+            string todayPath = string.Format("{0}/{1}", defaultPath, today);
+            logger.debug($"getSavePath: todayPath {todayPath}");
+            if (!writeable(todayPath))
+            {
+                logger.error($"getSavePath: unable to write todayPath {todayPath}");
+                return null;
+            }
+                
+            logger.debug($"getSavePath: returning writeable todayPath {todayPath}");
+            return savePath = todayPath;
         }
 
         // @see https://michaelridland.com/xamarin/creating-native-view-xamarin-forms-viewpage/
@@ -88,6 +130,28 @@ namespace EnlightenMobile.iOS
             nativeView.SetNeedsLayout();
      
             return nativeView;
+        }
+        bool writeable(string path)
+        {
+            logger.debug($"writeable: testing {path}");
+            if (Directory.Exists(path))
+                logger.debug($"exists: {path}");
+            else
+            {
+                logger.debug($"calling Mkdirs({path})");
+                Directory.CreateDirectory(path);
+                if (!Directory.Exists(path))
+                {
+                    logger.error($"writeable: Mkdirs failed to create {path}");
+                    return false;
+                }
+            }
+            // Before, and as can be seen in Android, these were File.Create
+            // iOS does not seem to like this for some reason and since they were
+            // directroies and not files I changed these to their respective dir commands
+            // and it now appears to work as intended
+
+            return true;
         }
     }
 }
